74936986e3f65abb623f16b938f23423336c5676
khanhvx2005 <kvuxuan789@gmail.com>
2025-12-31 13:52:34 +0700

    thay đổi trạng thái nhiều cv

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -4,5 +4,5 @@ const controller = require("../controllers/task.controller")
 router.get('/tasks', controller.index)
 router.get('/tasks/detail/:id', controller.detail)
 router.patch('/tasks/change-status/:id', controller.changeStatus)
-
+router.patch('/tasks/change-multi', controller.changeMulti)
 module.exports = router;
