998bbb227da2532517d3fdc69d5f00fb91a9cc14
khanhvx2005 <kvuxuan789@gmail.com>
2026-01-08 10:03:06 +0700

    cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 require("dotenv").config();
 const database = require("./config/database.config")
 database.connect()
 const app = express()
+app.use(cors())
+
 const port = process.env.PORT
 app.use(bodyParser.json())
 
-const route = require("./api/v1/routes/index.route")
-route(app)
+const routesApiVer1 = require("./api/v1/routes/index.route")
+routesApiVer1(app)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
