f60b1d7212c5689af97d4a090371032ea0f97383
khanhvx2005 <kvuxuan789@gmail.com>
2026-01-24 14:31:28 +0700

    Đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -27,3 +27,36 @@ module.exports.register = async (req, res) => {
     }
 
 }
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại !"
+        })
+        return;
+    }
+    if (md5(password) !== user.password) {
+        res.json({
+            code: 400,
+            message: "Sai mật khẩu !"
+        })
+        return;
+    }
+    if (user.status === "inactive") {
+        res.json({
+            code: 400,
+            message: "Tài khoản đã bị khóa !"
+        })
+        return;
+    }
+    res.cookie("token", user.token);
+    res.json({
+        code: 200,
+        message: "Đăng nhập thành công"
+    })
+}
